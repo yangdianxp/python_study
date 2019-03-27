e2f8c6569b6b670ecb826122f49b9ac1b210cbae
yangdianxp <yangdianxp@163.com>
2019-03-27 23:16:51 +0800

    ...

---
+++ vsproject/study_python/test.py
@@ -0,0 +1,62 @@
+a = True
+b = False
+print(b or 30)
+
+aaa = 40
+
+#import turtle
+#import math
+
+##定义多个点的坐标
+#x1, y1 = 100, 100
+#x2, y2 = 100, -100
+#x3, y3 = -100, -100
+#x4, y4 = -100, 100
+#turtle.penup()
+#turtle.goto(x1, y1)
+#turtle.pendown()
+#turtle.goto(x2, y2)
+#turtle.goto(x3, y3)
+#turtle.goto(x4, y4)
+#distance = math.sqrt((x1 - x4) ** 2 + (y1 - y4) ** 2)
+#turtle.write(distance)
+
+
+
+#import time
+
+#print(time.time())
+
+#a = 2
+#a **= 10
+#print(a)
+
+#print(round(3.5))
+#print(round(3.4599999999999999))
+
+#print(id(5))
+#print(id(0b101))
+#print(id(0o5))
+#print(id(0x5))
+
+#print(divmod(13, 3))
+
+
+#a = "我爱你"
+#print(id(a))
+#print(id("我爱你"))
+
+#a = 5
+#c = 5
+#print(id(4))
+#print(id(5))
+#print(id(a))
+#print(id(c))
+#print(type(5))
+#print(type(a))
+
+#import turtle
+#t = turtle.Pen()
+#for x in range(360):
+#    t.forward(x)
+#    t.left(59)
