ebae70f143dcbfac2f187fcbb5e6e4e448814b04
yangdianxp <yangdianxp@163.com>
2019-03-27 11:58:47 +0800

    ...

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-
-a = 5
+a = 3 + 5
+b = 8
+print(id(3))
+print(id(5))
+print(id(8))
 print(id(a))
-print(type(a))
+print(id(b))
+
+#a = 5
+#print(id(a))
+#print(type(a))
 
 #import turtle
 #t = turtle.Pen()
