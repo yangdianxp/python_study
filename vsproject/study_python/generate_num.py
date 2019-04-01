59333f44ccc918c6ef95d6abff3fca221e447537
yangdianxp <yangdianxp@163.com>
2019-04-01 13:59:30 +0800

    ...

--- vsproject/study_python/generate_num.py
+++ vsproject/study_python/generate_num.py
@@ -1,5 +1,5 @@
 f1 = open('mac.txt','w')
-num = 0x2435acd30000
+num = 0x001829BA0210
 for i in [num + x for x in range(5000)]:
-    f1.write(str(hex(i)).replace("0x", "").upper() + "\n")
+    f1.write("00" + str(hex(i)).replace("0x", "").upper() + "\n")
 f1.close()
