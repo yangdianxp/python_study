542f277aa0b84a9cde29cf2c4a9e4cbeeaa8b009
yangdianxp <yangdianxp@163.com>
2019-04-08 23:07:09 +0800

    ...

---
+++ vsproject/study_python/test_package/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="hm_message",
+      version="1.0",
+      description="发送接收消息模块",
+      long_description="完整的",
+      py_modules=["hm_message.send_message",
+                  "hm_message.recv_message"])
