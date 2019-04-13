bc04d381a051c146a0bb66bc663c17d6fa0f88da
yangdianxp <yangdianxp@163.com>
2019-04-13 14:26:43 +0800

    ...

--- vsproject/study_python/aircraft_war/plane_main.py
+++ vsproject/study_python/aircraft_war/plane_main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from plane_sprites import *
 
@@ -8,10 +9,14 @@ class PlaneGame:
         self.clock = pygame.time.Clock()
         self.__create_sprites()
         pygame.time.set_timer(CREATE_ENEMY_EVENT, 1000)
+        pygame.time.set_timer(HERO_FIRE_EVENT, 500)
     def __create_sprites(self):
         bg1 = Background() 
         bg2 = Background(True) 
         self.back_group = pygame.sprite.Group(bg1, bg2)
+        self.enemy_group = pygame.sprite.Group()
+        self.hero = Hero()
+        self.hero_group = pygame.sprite.Group(self.hero)
     def start_game(self):
         print("游戏开始")
         while True:
@@ -25,17 +30,40 @@ class PlaneGame:
             if event.type == pygame.QUIT:
                 PlaneGame.__game_over()
             elif event.type == CREATE_ENEMY_EVENT:
-                print("敌机出场。。。")
+                enemy = Enemy()
+                self.enemy_group.add(enemy)
+            elif event.type == HERO_FIRE_EVENT:
+                self.hero.fire()
+            #elif event.type == pygame.KEYDOWN and\
+            #    event.key == pygame.K_RIGHT:
+            #    print("向右移动。。。")
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_RIGHT]:
+            self.hero.speed = 8
+        elif keys_pressed[pygame.K_LEFT]:
+            self.hero.speed = -8
+        else:
+            self.hero.speed = 0
     def __check_collide(self):
-        pass
+        pygame.sprite.groupcollide(self.hero.bullets, self.enemy_group, True, True)
+        enemies = pygame.sprite.spritecollide(self.hero, self.enemy_group, True)
+        if len(enemies):
+            self.hero.kill()
+            PlaneGame.__game_over()
     def __update_spirites(self):
         self.back_group.update()
         self.back_group.draw(self.screen)
+        self.enemy_group.update()
+        self.enemy_group.draw(self.screen)
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
+        self.hero.bullets.update()
+        self.hero.bullets.draw(self.screen)
     @staticmethod
     def __game_over():
         print("游戏结束")
         pygame.quit()
-        exit()
+        sys.exit(0)
 
 if __name__ == '__main__':
     game = PlaneGame()
--- vsproject/study_python/aircraft_war/plane_sprites.py
+++ vsproject/study_python/aircraft_war/plane_sprites.py
@@ -1,9 +1,10 @@
-
+import random
 import pygame
 
 SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
 FRAME_PER_SEC = 60
 CREATE_ENEMY_EVENT = pygame.USEREVENT
+HERO_FIRE_EVENT = pygame.USEREVENT + 1
 
 class GameSprite(pygame.sprite.Sprite):
     """飞机大战游戏精灵"""
@@ -31,5 +32,51 @@ class Background(GameSprite):
 class Enemy(GameSprite):
     def __init__(self):
         super().__init__("./aircraft_war/images/enemy1.png")
+        self.speed = random.randint(1, 3)
+        self.rect.bottom = 0
+        max_x = SCREEN_RECT.width - self.rect.width
+        self.rect.x = random.randint(0, max_x)
+
     def update(self):
+        super().update()
+        if self.rect.y >= SCREEN_RECT.height:
+            #print("飞出屏幕，需要从精灵组删除。。。")
+            self.kill()
+
+    def __del__(self):
+        #print("敌机挂了 {}".format(self.rect))
+        pass
+
+class Hero(GameSprite):
+    """英雄精灵"""
+    def __init__(self):
+        super().__init__("./aircraft_war/images/me1.png")
+        self.rect.centerx = SCREEN_RECT.centerx
+        self.rect.bottom = SCREEN_RECT.bottom - 120
+
+        self.bullets = pygame.sprite.Group()
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.x < 0:
+            self.rect.x = 0
+        elif self.rect.right > SCREEN_RECT.right:
+            self.rect.right = SCREEN_RECT.right
+
+    def fire(self):
+        for i in (0, 20, 40):
+            bullet = Bullet()
+            bullet.rect.bottom = self.rect.y - i
+            bullet.rect.centerx = self.rect.centerx
+            self.bullets.add(bullet)
+
+class Bullet(GameSprite):
+    """子弹精灵"""
+    def __init__(self):
+        super().__init__("./aircraft_war/images/bullet1.png", -2)
+    def update(self):
+        super().update()
+        if self.rect.bottom < 0:
+            self.kill()
+    def __del__(self):
+        #print("子弹被销毁")
         pass
