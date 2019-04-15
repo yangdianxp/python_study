ab52afc5fb9fc4331f3b2ceaf4c74fc7eb7792b5
yangdianxp <yangdianxp@163.com>
2019-04-16 00:02:19 +0800

    ...

--- vsproject/study_python/multitasking/coroutine_study1.py
+++ vsproject/study_python/multitasking/coroutine_study1.py
@@ -7,11 +7,33 @@ def create_num(all_num):
         yield a  
         a, b = b, a + b
         current_num += 1
+    return "yangdian"
 
-obj = create_num(10)
-print(obj)
+def create_num2(all_num):
+    for i in range(all_num):
+        ret = yield i
+        print(">>>>>ret>>>>>", ret)
 
-ret = next(obj)
+#obj = create_num(10)
+#print(obj)
+
+#ret = next(obj)
+#print(ret)
+
+#obj2 = create_num(100)
+
+#while True:
+#    try:
+#        ret = next(obj2)
+#        print(ret)
+#    except Exception as ret:
+#        print(ret.value)
+#        break
+
+obj3 = create_num2(100)
+ret = obj3.send(None)
+print(ret)
+ret = obj3.send("yangdian")
 print(ret)
 
 #for i in obj:
