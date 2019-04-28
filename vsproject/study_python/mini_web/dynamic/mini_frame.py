f9fa113bd55ce2d32d3732e717c7262907cf414f
yangdianxp <yangdianxp@163.com>
2019-04-28 21:45:53 +0800

    ...

--- vsproject/study_python/mini_web/dynamic/mini_frame.py
+++ vsproject/study_python/mini_web/dynamic/mini_frame.py
@@ -13,19 +13,27 @@ def route(file_name):
 @route("/index.py")
 def index():
     with open("./mini_web/templates/index.html", encoding='utf-8') as f:
-        return f.read()
+        content = f.read()
+    my_stock_info = "哈哈哈哈 这是你的本月名称......"
+    content = re.sub(r"\{%content%\}", my_stock_info, content)
+    return content
 
 @route("/center.py")
 def center():
     with open("./mini_web/templates/center.html", encoding='utf-8') as f:
-        return f.read()
+        content = f.read()
+    my_stock_info = "这里是从mysql查询出来的数据..."
+    content = re.sub(r"\{%content%\}", my_stock_info, content)
+    return content
 
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html;charset=utf-8')])
     file_name = env['PATH_INFO']
 
-    if file_name in URL_FUNC_DICT:
-        return URL_FUNC_DICT[file_name]()
-    else:
-        return 'Hello World 我爱你'
+    try: 
+        func = URL_FUNC_DICT[file_name]
+        return func()
+    except Exception as ret:
+        return "产生了异常: {}".format(ret)
+
 
