5da1334f59ef94860471dd78a09dd79f355f6f83
yangdianxp <yangdianxp@163.com>
2019-05-01 00:28:18 +0800

    ...

--- vsproject/study_python/mini_web/dynamic/mini_frame.py
+++ vsproject/study_python/mini_web/dynamic/mini_frame.py
@@ -14,8 +14,15 @@ def route(file_name):
 def index():
     with open("./mini_web/templates/index.html", encoding='utf-8') as f:
         content = f.read()
-    my_stock_info = "哈哈哈哈 这是你的本月名称......"
-    content = re.sub(r"\{%content%\}", my_stock_info, content)
+    #my_stock_info = "哈哈哈哈 这是你的本月名称......"
+    #content = re.sub(r"\{%content%\}", my_stock_info, content)
+    conn = connect(host = 'localhost', port = 3306, user = 'root', database = 'stock_db', charset = 'utf8')
+    cs = conn.cursor()
+    cs.execute("select * from info;")
+    stock_infos = cs.fetchall()
+    cs.close()
+    conn.close()
+    content = re.sub(r"\{%content%\}", str(stock_infos), content)
     return content
 
 @route("/center.html")
