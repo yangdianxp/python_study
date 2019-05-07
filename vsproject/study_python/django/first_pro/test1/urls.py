b4d40e82b3a3c3eb139bbc22cd975702df4eddcf
yangdianxp <yangdianxp@163.com>
2019-05-07 23:41:33 +0800

    django study

--- vsproject/study_python/django/first_pro/test1/urls.py
+++ vsproject/study_python/django/first_pro/test1/urls.py
@@ -4,4 +4,5 @@ from test1 import views
 urlpatterns = [
     re_path(r'^index$', views.index),
     re_path(r'^index2$', views.index2),
+    re_path(r'^books$', views.show_books),
 ]
--- vsproject/study_python/django/first_pro/test1/views.py
+++ vsproject/study_python/django/first_pro/test1/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader,RequestContext
+from test1.models import BookInfo 
+
+#def my_render(template_path, context_dict = {}):
+#    temp = loader.get_template(template_path)
+#    #模板渲染：产生标准的html内容
+#    return HttpResponse(temp.render(context_dict))
 
 # Create your views here.
 # 定义视图函数
 def index(request):
-    # 进行处理，和M和T进行交互
-    return HttpResponse('老铁，没毛病')
+    return render(request, "test1/index.html", 
+                  {"content":"hello world",
+                   "list":list(range(1, 10))})
+
 
 def index2(request):
     return HttpResponse('hello python')
+
+def show_books(request):
+    books = BookInfo.objects.all()
+    return render(request, 'test1/show_books.html', {'books':books})
