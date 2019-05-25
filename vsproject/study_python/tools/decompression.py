18e42e45c4951f56dceea32708e4fa1f1c7c66fb
yangdianxp <yangdianxp@163.com>
2019-05-25 11:05:45 +0800

    ...

---
+++ vsproject/study_python/tools/decompression.py
@@ -0,0 +1,27 @@
+import traceback
+import os
+import sys
+import zipfile
+
+def main():
+    #查找文件名
+    os.chdir(r"C:\Users\Administrator\Desktop\temp")
+    for root, dirs, files in os.walk("."):
+        for f in files:             
+            unzip = zipfile.ZipFile(f)
+            for u in unzip.namelist():
+                unzip.extract(u, ".")
+                print('uncompressing {} finished'.format(u))
+            unzip.close()
+            os.remove(f)
+            
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as result:
+        ex_type, ex_val, ex_stack = sys.exc_info()
+        print(ex_type)
+        print(ex_val)
+        for stack in traceback.extract_tb(ex_stack):
+            print(stack)
+
