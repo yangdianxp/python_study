b4783318867c48b0eeb8c3cf50a449e91323278a
yangdianxp <yangdianxp@163.com>
2019-09-19 13:32:40 +0800

    Update test1.py

--- vsproject/study_python/test1.py
+++ vsproject/study_python/test1.py
@@ -1,4 +1,150 @@
-# 例
+# 例 
+def make_averager():
+    series = []
+    def averager(new_value):
+        series.append(new_value)
+        total = sum(series)
+        return total/len(series)
+    return averager
+
+avg = make_averager()
+print(avg.__code__)
+
+#b = 6
+#def f3(a):
+#    global b
+#    print(a)
+#    print(b)
+#    b = 9
+
+#promos = []
+#def promotion(promo_func):
+#    promos.append(promo_func)
+#    return promo_func
+
+#@promotion
+#def fidelity(order):
+#    """为积分为1000或以上的顾客提供5%折扣"""
+#    return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+#@promotion
+#def bulk_item(order):
+#    """单个商品为20个或以上时提供10%折扣"""
+#    discount = 0
+#    for item in order.cart:
+#        if item.quantity >= 20:
+#            discount += item.total() * .1
+#    return discount
+
+#@promotion
+#def large_order(order):
+#    """订单中的不同商品达到10个或以上时提供7%折扣"""
+#    distinct_items = {item.product for item in order.cart}
+#    if len(distinct_items) >= 10:
+#        return order.total() * .07
+#    return 0
+
+#def best_promo(order):
+#    """选择可用的最佳折扣 """
+#    return max(promo(order) for promo in promos)
+
+#registry = []
+#def register(func):
+#    print('running register(%s)' % func)
+#    registry.append(func)
+#    return func
+
+#@register
+#def f1():
+#    print('running f1()')
+
+#@register
+#def f2():
+#    print('running f2()')
+
+#def f3():
+#    print('running f3()')
+
+#def main():
+#    print('running main()')
+#    print('registry ->', registry)
+#    f1()
+#    f2()
+#    f3()
+
+#if __name__=='__main__':
+#    main()
+
+#def deco(func):
+#    def inner():
+#        print('running inner()')
+#    return inner
+#@deco
+#def target():
+#    print('running target()')
+#target()
+
+
+## 把函数当成一等对象，实现策略模式
+## 一等对象定义：
+## 在运行时创建
+## 能赋值给变量或数据结构中的元素
+## 能作为参数传给函数
+## 能作为函数的返回结果
+
+#from collections import namedtuple
+#Customer = namedtuple('Customer', 'name fidelity')
+#class LineItem:
+#    def __init__(self, product, quantity, price):
+#        self.product = product
+#        self.quantity = quantity
+#        self.price = price
+#    def total(self):
+#        return self.price * self.quantity
+
+#class Order: # 上下文
+#    def __init__(self, customer, cart, promotion=None):
+#        self.customer = customer
+#        self.cart = list(cart)
+#        self.promotion = promotion
+#    def total(self):
+#        if not hasattr(self, '__total'):
+#            self.__total = sum(item.total() for item in self.cart)
+#            return self.__total
+#    def due(self):
+#        if self.promotion is None:
+#            discount = 0
+#        else:
+#            discount = self.promotion(self)
+#        return self.total() - discount
+#    def __repr__(self):
+#        fmt = '<Order total: {:.2f} due: {:.2f}>'
+#        return fmt.format(self.total(), self.due())
+
+#def fidelity_promo(order):
+#    """为积分为1000或以上的顾客提供5%折扣"""
+#    return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+#def bulk_item_promo(order):
+#    """单个商品为20个或以上时提供10%折扣"""
+#    discount = 0
+#    for item in order.cart:
+#        if item.quantity >= 20:
+#            discount += item.total() * .1
+#    return discount
+
+#def large_order_promo(order):
+#    """订单中的不同商品达到10个或以上时提供7%折扣"""
+#    distinct_items = {item.product for item in order.cart}
+#    if len(distinct_items) >= 10:
+#        return order.total() * .07
+#    return 0
+
+#promos = [fidelity_promo, bulk_item_promo, large_order_promo]
+#def best_promo(order):
+#    """选择可用的最佳折扣 """
+#    return max(promo(order) for promo in promos)
+
 
 
 #import random
