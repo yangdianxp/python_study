81cb721a503155b53427d8b6a08adb4f7b47b23a
yangdianxp <yangdianxp@163.com>
2019-09-19 14:21:15 +0800

    Update test1.py

--- vsproject/study_python/test1.py
+++ vsproject/study_python/test1.py
@@ -1,14 +1,75 @@
 # 例 
-def make_averager():
-    series = []
-    def averager(new_value):
-        series.append(new_value)
-        total = sum(series)
-        return total/len(series)
-    return averager
+import time
+import functools
+def clock(func):
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - t0
+        name = func.__name__
+        arg_lst = []
+        if args:
+            arg_lst.append(', '.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_lst.append(', '.join(pairs))
+        arg_str = ', '.join(arg_lst)
+        print('[%0.8fs] %s(%s) -> %r ' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
 
-avg = make_averager()
-print(avg.__code__)
+
+#import time
+#def clock(func):
+#    def clocked(*args): # ➊
+#        t0 = time.perf_counter()
+#        result = func(*args) # ➋
+#        elapsed = time.perf_counter() - t0
+#        name = func.__name__
+#        arg_str = ', '.join(repr(arg) for arg in args)
+#        print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+#        return result
+#    return clocked # ➌
+
+#@clock
+#def snooze(seconds):
+#    time.sleep(seconds)
+
+#@clock
+#def factorial(n):
+#    return 1 if n < 2 else n*factorial(n-1)
+
+#if __name__=='__main__':
+#    print('*' * 40, 'Calling snooze(.123)')
+#    snooze(.123)
+#    print('*' * 40, 'Calling factorial(6)')
+#    print('6! =', factorial(6))
+
+#def make_averager():
+#    count = 0
+#    total = 0
+#    def averager(new_value):
+#        nonlocal count, total
+#        count += 1
+#        total += new_value
+#        return total / count
+#    return averager
+#avg = make_averager()
+#print(avg(10))
+
+
+#def make_averager():
+#    series = []
+#    def averager(new_value):
+#        series.append(new_value)
+#        total = sum(series)
+#        return total/len(series)
+#    return averager
+
+#avg = make_averager()
+#print(avg.__code__.co_varnames)
+#print(avg.__code__.co_freevars)
 
 #b = 6
 #def f3(a):
