900e28a668094c74e7cc5ee35cb3d993bb7d86a7
yangdianxp <yangdianxp@163.com>
2019-09-19 18:04:08 +0800

    Update test1.py

--- vsproject/study_python/test1.py
+++ vsproject/study_python/test1.py
@@ -1,5 +1,45 @@
 # 例 
+registry = set() #➊
+def register(active=True): #➋
+    def decorate(func): #➌
+        print('running register(active=%s)->decorate(%s)'
+            % (active, func))
+        if active: #➍
+            registry.add(func)
+        else:
+            registry.discard(func) #➎
+        return func #➏
+    return decorate #➐
 
+@register(active=False) #➑
+def f1():
+    print('running f1()')
+@register() #➒
+def f2():
+    print('running f2()')
+def f3():
+    print('running f3()')
+
+#from functools import singledispatch
+#from collections import abc
+#import numbers
+#import html
+#@singledispatch
+#def htmlize(obj):
+#    content = html.escape(repr(obj))
+#    return '<pre>{}</pre>'.format(content)
+#@htmlize.register(str)
+#def _(text):
+#    content = html.escape(text).replace('\n', '<br>\n')
+#    return '<p>{0}</p>'.format(content)
+#@htmlize.register(numbers.Integral)
+#def _(n):
+#    return '<pre>{0} (0x{0:x})</pre>'.format(n)
+#@htmlize.register(tuple)
+#@htmlize.register(abc.MutableSequence)
+#def _(seq):
+#    inner = '</li>\n<li>'.join(htmlize(item) for item in seq)
+#    return '<ul>\n<li>' + inner + '</li>\n</ul>'
 
 
 #import time
