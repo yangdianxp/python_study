1d53469dfcb7e4a568df42033e1d87f96f087934
yangdianxp <41214656+yangdianxp@users.noreply.github.com>
2020-01-13 16:02:37 +0800

    ...

--- vsproject/study_python/test.py
+++ vsproject/study_python/test.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import codecs
+url = 'https://t66y.com/thread0806.php?fid=15&search=&page='
+browser = webdriver.Chrome()
+file_output = codecs.open('thefile.csv','w', 'utf_8_sig')
+for i in range(1,100):
+    browser.get(url+str(i))
+    List = browser.find_elements_by_xpath("//h3/a[@target='_blank']")
+    for x in List:
+        file_output.write(x.text+','+x.get_attribute('href'))
+file_output.close()
+
+
 #words = [
 #    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
 #    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
