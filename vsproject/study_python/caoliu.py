25eb9a040ac698635691269727bb6d6ce9403520
yangdianxp <41214656+yangdianxp@users.noreply.github.com>
2020-01-15 22:20:06 +0800

    ...

--- vsproject/study_python/caoliu.py
+++ vsproject/study_python/caoliu.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import codecs
-url = 'https://t66y.com/thread0806.php?fid=15&search=&page='
+url = 'https://cc.yjan.icu/thread0806.php?fid=25&search=&page='
 browser = webdriver.Chrome()
 file_output = codecs.open('thefile.csv','w', 'utf_8_sig')
-for i in range(1,100):
+for i in range(55,101):
     browser.get(url+str(i))
     List = browser.find_elements_by_xpath("//h3/a[@target='_blank']")
     for x in List:
